edd017bf9ee95358222a2f53b4012de1a8187b20
Miguel Guadarrama <mguadarrama@gcfund.org>
2020-06-23 16:19:24 +0900

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -78,8 +78,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          Powered by Vercel
         </a>
       </footer>
 
